8227b6f4d24d087f961cfad303342f0de5a98a5e
Md Arif Jahan <56018713+arifjahan88@users.noreply.github.com>
2025-10-09 13:51:27 +0600

    fix front issue

--- excel_rma/public/rma-web/assets/app-strings-D56g2F-8.js
+++
@@ -1,1 +0,0 @@
-import{U as e}from"./index-K-zqVkRk.js";const a=[{value:"completed",label:"Completed"},{value:"cancelled",label:"Cancelled"},{value:"submitted",label:"Submitted"}],t="Excel technologies Limited",o=["item_name","serial_no"],s=e.LIST_IGNORE;export{s as A,t as C,a as P,o as U};
